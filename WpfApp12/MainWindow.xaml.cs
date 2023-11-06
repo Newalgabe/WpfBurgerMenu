b47809ae4a62ea2055c03f333146f5ed8641e779
newal <newal@NICAT>
2023-11-07 00:49:37 +0400

    Add project files.

---
+++ WpfApp12/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace WpfApp12
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BurgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MenuPanel.Margin.Left < 0)
+            {
+                Storyboard openMenuAnimation = this.FindResource("OpenMenuAnimation") as Storyboard;
+                Storyboard.SetTarget(openMenuAnimation, MenuPanel);
+                openMenuAnimation.Begin();
+            }
+            else
+            {
+                Storyboard closeMenuAnimation = this.FindResource("CloseMenuAnimation") as Storyboard;
+                Storyboard.SetTarget(closeMenuAnimation, MenuPanel);
+                closeMenuAnimation.Begin();
+            }
+        }
+    }
+}
+
